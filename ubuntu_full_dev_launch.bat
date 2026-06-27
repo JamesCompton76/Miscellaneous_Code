e4b991ca51ce8b4c1ec48e4e009d7bcff8603f40
JamesCompton76 <72444873+JamesCompton76@users.noreply.github.com>
2026-06-27 16:52:46 +0100

    Add files via upload

---
+++ ubuntu_full_dev_launch.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\WSL\wsl.exe" --distribution-id {b3c166ea-97b5-46d8-ab70-769e8adb311f} --cd ~ -e bash -c "echo 'source ~/.bashrc; source .venv/bin/activate; code .' > /tmp/wsl_init.sh && bash --rcfile /tmp/wsl_init.sh -i"
